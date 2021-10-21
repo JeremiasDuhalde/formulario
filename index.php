3690f629c53df0892dadbe1e6603040058cbf05e
crushtkd <crush.tkd@gmail.com>
2021-10-21 01:25:14 -0300

    validaciones

--- index.php
+++ index.php
@@ -1,113 +1,153 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>formulario</title>
     <meta charset="UTF-8">
     <!--<link rel="stylesheet" type="text/css" href="css/estilo.css">-->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
+    <!-- Default theme -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
 </head>
+
 <body>
 
-<div class="container">
-    <div class="row">
-        <br><br>
-        <div class="panel panel-default">
-            <div class="panel-heading">Formulario</div>
-            <div class="panel-body">
+    <div class="container">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">Formulario</div>
+                    <div class="panel-body">
+                        <form action="php/guardar.php" method="post">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label> Nombre (*)</label>
+                                    <input type="text" class="form-control" name="nombre" id="nombre" required>
+                                    <div class="nombre">
+                                        <span class="text-danger" hidden></span>
+                                    </div>
+                                </div>
+                            </div>
 
-                <form action="php/guardar.php" method="post">
-               
-		  <div class="col-md-4">
-                        <div class="form-group">
-                            <label> Nombre (*)</label>
-                            <input type="text" class="form-control" placeholder="Nombre" name="nombre">
-                        	</div></div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Apellido (*)</label>
+                                    <input type="text" class="form-control" name="apellido" id="apellido" required>
+                                    <div class="apellido">
+                                        <span class="text-danger" hidden></span>
+                                    </div>
+                                </div>
+                            </div>
 
-  		  <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Apellido (*)</label>
-                            <input type="text" class="form-control" placeholder="Apellido" name="apellido">
-                     	   	</div></div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Username (*)</label>
+                                    <input type="email" class="form-control" name="username" id="username" required>
+                                    <div class="username">
+                                        <span class="text-danger" hidden></span>
+                                    </div>
+                                </div>
+                            </div>
 
-                 <div class="col-md-4">
-                        <div class="form-group">
-                            <label>Username (*)</label>
-                            <input type="email" class="form-control" placeholder="example@example.com" name="username">
-                       		 </div></div>
-				
- 		 <div class="col-md-4">
-    			 <div class="form-group">
-                            <label>Contraseña (*)</label>
-                            <input type="text" class="form-control" placeholder="*******" name="password">
-                        	</div></div>
-		<div class="col-md-6">
-                  	 <div class="form-group">
-                            <label>Provincia (*)</label>
-                            <input type="number" class="form-control" placeholder="..." name="telefono">
-                        </div></div>
-                  	<div class="col-md-6">
-               		  <div class="form-group">
-                            <label>Opciones</label><br>
-              
-                                <input type="checkbox" value="Terminos" name="opcion[]"/>Acepta terminos y condiciones<br>
-              
-                            </div></div>
-                    
-	<div class="col-md-11">
-                        <div class="form-group">
-                            <label for="fecha">fecha/hora</label>
-                            <input type="datetime-local"  class="form-control" id="fecha" name="fecha" value="fecha">
-                        </div>   </div>
-                        <input class="btn btn-primary btn-block" type="submit" value="enviar">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Contraseña (*)</label>
+                                    <input type="text" class="form-control" name="password" id="password" required>
+                                    <div class="password">
+                                        <span class="text-danger" hidden></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Provincia (*)</label>
+                                    <input type="number" class="form-control" placeholder="..." name="telefono" id="telefono" required>
+                                    <div class="telefono">
+                                        <span class="text-danger" hidden></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Opciones</label><br>
+
+                                    <input type="checkbox" value="Terminos" name="terminos" id="terminos" />Acepta terminos y condiciones<br>
+
+                                </div>
+                            </div>
+
+                            <div class="col-md-11">
+                                <div class="form-group">
+                                    <label for="fecha">fecha/hora</label>
+                                    <input type="date" class="form-control" id="fecha" name="fecha" required>
+                                </div>
+                            </div>
+                            <input class="btn btn-primary btn-block" disabled type="button" value="enviar" id="registrar">
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<!--
-<div class="form">
-    <form action="php/guardar.php" method="POST">
-        <label for="nombre">Nombre</label>
-        <br>
-        <input type="text" id="nombre" name="nombre" placeholder="nombre">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
-        <label for="apellido">Apellido</label>
-        <br>
-        <input type="text" id="apellido" name="apellido" placeholder="apellido">
+    <script>
+        $(document).ready(function() {
+            $('#registrar').click(function() {
+                if ($('#password').val() == "") {
 
-        <label for="usuario">Usuario</label>
-        <br>
-        <input type="email" id="usuario" name="usuario" placeholder="example@example.com">
-  
-        <label for="password">Contraseña</label>
-        <br>
-        <input type="text" id="password" name="password" placeholder="password">
-        <br>
+                    alertify.alert("Información:", "El campo CONTRASEÑA no debe estar vacio");
 
-        <label for="opciones">opciones</label>
-        <br>
-        <td>
-            <input type="checkbox" value="Terminos" name="opcion[]"/>Terminos
-            <br>
-        </td>
-        <br>
+                } else if ($('#username').val() == "") {
 
-        <label for="fecha">fecha/hora</label>
-        <div>
-            <input type="datetime-local" id="fecha" name="fecha" value="fecha">
-        </div>
-        <br>
+                    alertify.alert("Información:", "El campo USERNAME no debe estar vacio");
 
+                } else if ($('#telefono').val() == "") {
 
-        <input type="submit" value="enviar">
+                    alertify.alert("Información:", "El campo TELEFONO no debe estar vacio");
+                    
+                } else if ($('#fecha').val() == "") {
 
-    </form>
-</div>-->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+                    alertify.alert("Información:", "El campo FECHA no debe estar vacio");
+
+                }else {
+                    alertify.alert("Información:", "Todos los datos son correctos");
+
+                    
+
+                    // cadena = "user=" + $('#usuario').val() +
+                    //     "&pass=" + $('#password').val() +
+                    //     "&email=" + $('#campeonato').val();
+
+                    // $.ajax({
+                    //     type: "POST",
+                    //     url: "php/login.php",
+                    //     data: cadena,
+                    //     success: function(r) {
+                    //         if (r == 1) {
+                    //             window.location = "academia.php";
+                    //         } else {
+                    //             alertify.alert("Organizacion informa:", "Usuario o Contraseña incorrecto");
+                    //         }
+                    //     }
+                    // });
+                }
+
+            });
+            $('#terminos').on('checked', function(){
+                $('#registrar').removeAttr('disabled','disabled');
+            })
+        });
+    </script>
+
 </body>
+
 </html>
